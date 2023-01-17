0a8a6b0aaa3deaf464bf5abd866cd476522e9866
Bma52 <100071125+Bma52@users.noreply.github.com>
2023-01-17 16:54:43 +0200

    Update ActionRec_web_app.py

--- ActionRec_web_app.py
+++ ActionRec_web_app.py
@@ -119,9 +119,9 @@ def get_new_reviews_mysql() -> pd.DataFrame:
    
    
    
-    df_1 = product_data.merge(review_data, how = 'right', on = 'product_name', suffixes=('', '_DROP')).filter(regex='^(?!.*_DROP)')
+    df_1 = product_data.merge(review_data, how = 'inner', on = 'product_name', suffixes=('', '_DROP')).filter(regex='^(?!.*_DROP)')
     df_product = pd.DataFrame(df_1)
-    df_full = df_product .merge(final_annotation_data, how = 'right', on='reviewBody', suffixes=('', '_DROP')).filter(regex='^(?!.*_DROP)')
+    df_full = df_product .merge(final_annotation_data, how = 'inner', on='reviewBody', suffixes=('', '_DROP')).filter(regex='^(?!.*_DROP)')
       
       
       
