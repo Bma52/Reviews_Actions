a22442ee276e2918e0afa85491cfa7d41d5b3167
Bma52 <100071125+Bma52@users.noreply.github.com>
2023-01-18 11:20:48 +0200

    Update ActionRec_web_app.py

--- ActionRec_web_app.py
+++ ActionRec_web_app.py
@@ -698,7 +698,6 @@ def main(df_product, df_review, df_annotation) -> None:
           #placeholder = st.empty()
             
           df_one_review = df_annotation.loc[df_annotation['reviewBody'] == i]
-          st.write(df_one_review)
           st.markdown('<p style="font-family:sans-serif; color:Red; font-size: 20px;">Product Name:</p>', unsafe_allow_html=True)
           st.subheader(df_review["product_name"][df_review["reviewBody"] == i].unique())
           st.markdown('<p style="font-family:sans-serif; color:Red; font-size: 20px;">Review Text:</p>', unsafe_allow_html=True)
@@ -729,7 +728,7 @@ def main(df_product, df_review, df_annotation) -> None:
     for i in list_reviews:
         review_container(i)
         
-        submit_btn = st.button("Submit Review", key = df_review["review_id"][df_review["reviewBody"] == i)
+        submit_btn = st.button("Submit Review", key = df_review["review_id"][df_review["reviewBody"] == i])
         #next_btn = st.button("Next Review", key = df_one_review["reviewBody"].unique())
         
         if submit_btn:
