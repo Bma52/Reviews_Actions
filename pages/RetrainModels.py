9784b2ac8dc01a67c410562e00c56bcc77a36c4c
Bma52 <100071125+Bma52@users.noreply.github.com>
2023-01-24 22:15:37 +0200

    Update RetrainModels.py

--- pages/RetrainModels.py
+++ pages/RetrainModels.py
@@ -1,1 +1,104 @@
 
+from lib2to3.pgen2.pgen import DFAState
+import streamlit as st
+#from st_aggrid import AgGrid
+#from st_aggrid.shared import JsCode
+#from st_aggrid.grid_options_builder import GridOptionsBuilder
+import pandas as pd
+import plotly as plt
+import plotly.express as px
+#from joblib import load
+import pickle
+import numpy as np
+import time
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.model_selection import train_test_split 
+#from imblearn.under_sampling import RandomUnderSampler
+from sklearn.svm import SVC
+from sklearn import preprocessing
+from xgboost import XGBClassifier
+from sklearn import linear_model
+from sklearn.preprocessing import LabelEncoder
+import hashlib
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+import operator 
+from heapq import nlargest
+import time
+import json
+import csv
+from SPARQLWrapper import SPARQLWrapper, JSON
+import ssl
+import streamlit_authenticator as stauth
+import yaml
+from streamlit_authenticator import hasher 
+import pymysql
+import functools
+import datetime
+import pandas as pd 
+import numpy as np 
+import requests
+import json
+import mysql.connector
+from mysql.connector import errorcode
+import nltk.data
+from tables import index
+from sklearn import preprocessing
+import jprops
+from jproperties import Properties
+import operator 
+from heapq import nlargest
+from sqlalchemy import create_engine
+import sqlalchemy 
+
+
+
+
+
+def get_model_info_mysql():
+   
+
+    host="linked.aub.edu.lb"
+    port=3306
+    database ="reviews_actions_ml"
+
+
+    
+    configs = Properties()
+    
+    with open('dbconfig.properties', 'rb') as config_file:
+         configs.load(config_file)
+           
+    dbConnection = mysql.connector.connect(user=configs.get("db.username").data, password=configs.get("db.password").data, host="linked.aub.edu.lb", database="reviews_actions_ml")
+
+
+
+    model_data = pd.read_sql_query("SELECT * FROM ML_models", dbConnection)
+
+      
+    
+    dbConnection.commit()
+   
+    
+
+    return  model_data
+  
+  
+  
+def main():
+    model_data = get_model_info_mysql()
+    with st.expander("View ML Models Information"):
+        st.write(model_data)
+   
+  
+  
+  
+  
+if __name__ == "__main__":
+    
+    main()
+  
+  
+  
+  
+  
