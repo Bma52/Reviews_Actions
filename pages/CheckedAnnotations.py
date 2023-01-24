83bb0e569856b264ee37c950cb33ea1a016c4690
Bma52 <100071125+Bma52@users.noreply.github.com>
2023-01-24 21:08:12 +0200

    Update CheckedAnnotations.py

--- pages/CheckedAnnotations.py
+++ pages/CheckedAnnotations.py
@@ -335,7 +335,7 @@ def main():
       
      with st.expander("View Checked Annotation"):
           #st.write(checked_data)
-       for j in len(checked_data.shape[0]):
+       for j in range(0,checked_data.shape[0]):
           col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12 = st.columns(12)
           with col1:
               st.write(checked_data["annotation"])
