bacee54c4a2f65d334da3042c74b33561f53b323
Bma52 <100071125+Bma52@users.noreply.github.com>
2023-01-25 08:53:15 +0200

    Update AnnotatorPage.py

--- pages/AnnotatorPage.py
+++ pages/AnnotatorPage.py
@@ -518,12 +518,12 @@ def main(df_annotation, annotator_name) -> None:
     for i in list_reviews:
         
         review_container(i, annotator_name)
-        load_next_btn = st.button("Load Next Review", key = df_annotation["review_id"][df_annotation["reviewBody"] == i])
+    load_next_btn = st.button("Load Next Review", key = df_annotation["review_id"][df_annotation["reviewBody"] == i])
          
 
-        if load_next_btn:
+    if load_next_btn:
               continue;  
-        else:
+    else:
               break;
           
 
