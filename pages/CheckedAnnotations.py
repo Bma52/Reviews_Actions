0558b6aeca8b042155ff7ee4122aaa5f20a8236b
Bma52 <100071125+Bma52@users.noreply.github.com>
2023-01-26 15:34:38 +0200

    Update CheckedAnnotations.py

--- pages/CheckedAnnotations.py
+++ pages/CheckedAnnotations.py
@@ -104,7 +104,7 @@ def insert_to_sparql(df_tuples, annotation_md5):
               )
 
 
-          sparql.setQuery(queryString) 
+          sparql.setQuery(str(queryString)) 
           sparql.method = 'POST'
           sparql.query()
     st.write("Successfully inserted into triple store.")
