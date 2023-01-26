66a504852888f9812bf50d17f324cc532604f83c
Bma52 <100071125+Bma52@users.noreply.github.com>
2023-01-26 12:07:44 +0200

    Update AnnotatorPage.py

--- pages/AnnotatorPage.py
+++ pages/AnnotatorPage.py
@@ -152,7 +152,7 @@ def insert_checked_annotation(df):
         df["Object"] =  df["Object"].astype(str)
         df["Ability"] = df["Ability"].astype(str)
         df["User_description"] = df["User_description"].astype(str)
-        df["annotation_md5"] = df["annotation_md5"].astype(str)
+        #df["annotation_md5"] = df["annotation_md5"].astype(str)
         df["checkedBy"] = df["checkedBy"].astype(str)
       
       
