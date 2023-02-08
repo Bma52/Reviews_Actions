c3e4711e0717d5504952c1df858c761b907a73c2
Bma52 <100071125+Bma52@users.noreply.github.com>
2023-02-08 19:53:31 +0200

    Update CheckedAnnotations.py

--- pages/CheckedAnnotations.py
+++ pages/CheckedAnnotations.py
@@ -376,9 +376,9 @@ def main():
     
         reviews = checked_data["annotation"].unique()
         for review in reviews:
-           checked_data = checked_data[checked_data["annotation"] == review]
+           
            with st.expander("View Checked Annotation"):
-           #st.write(checked_data)
+              checked_data = checked_data[checked_data["annotation"] == review]
               for j in checked_data.index:
                  col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12, col13 = st.columns(13)
                  with col1:
