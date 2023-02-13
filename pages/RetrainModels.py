bbcf1a170c635441aa10752f4db33218d314ea50
Bma52 <100071125+Bma52@users.noreply.github.com>
2023-02-13 18:13:05 +0200

    Update RetrainModels.py

--- pages/RetrainModels.py
+++ pages/RetrainModels.py
@@ -117,7 +117,7 @@ def store_new_model_data(model_version, label, accuracy):
     accuracy = float(accuracy)
     
     cursor = dbConnection.cursor()
-    sql = "INSERT INTO `ML_models` (model_version, label, accuracy) VALUES ("+ model_version + "," + label + "," + accuracy ");"
+    sql = "INSERT INTO `ML_models` (model_version, label, accuracy) VALUES ("+ model_version + "," + label + "," + accuracy + ");"
    
     cursor.execute(sql)
     st.write("New Model Information is now stored in MYSQL")
