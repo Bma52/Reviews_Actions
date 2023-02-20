28cb95b17bd475509f0206590c2bd13ecace91b2
Bma52 <100071125+Bma52@users.noreply.github.com>
2023-02-20 22:05:38 +0200

    Update RetrainModels.py

--- pages/RetrainModels.py
+++ pages/RetrainModels.py
@@ -145,7 +145,10 @@ def update_to_git(model, filename_str):
     gh = login(username=username, password=password)
     repo = gh.repository(account, repo)
     filename = filename_str
-    repo.create_file(filename_str, 'Model Updated', pickle.dump(model, open(filename, 'wb')), branch='main')
+    #repo.create_file(filename_str, 'Model Updated', pickle.dump(model, open(filename, 'wb')), branch='main')
+    
+    contents = repo.get_contents(filename_str)
+    repo.update_file(contents.path, "more training set", "more training set", pickle.dump(model, open(filename, 'wb')), branch='main')
     #new_content = pickle.dump(model, open(filename, 'wb'))
     
     #with open(filename, 'rb') as fd:
